9c4a3033674ec9e27332b1f4eef6b20aa771d91e
mickey-40 <mickeyarnold53@yahoo.com>
2023-03-29 12:45:14 -0500

    setup basic navbar with cart icon

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
+import { IoIosCart } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">Helmet</Link>
+      </p>
+      <button type='button' className='cart-icon' onClick=''>
+        <IoIosCart />
+        <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
   )
 }
 
