383675507e13ddcd4701b8a9ba672a928ca6b06d
mickey-40 <mickeyarnold53@yahoo.com>
2023-03-30 19:19:42 -0500

    commented out small images under product to fix bugs

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -17,7 +17,7 @@ const ProductDetails = ({ product, products}) => {
             className="product-detail-image"
             />
         </div>
-        <div className="small-images-container">
+        {/* <div className="small-images-container">
           {image?.map((item, i) => (
             <img   
               key={i} 
@@ -27,7 +27,7 @@ const ProductDetails = ({ product, products}) => {
               onMouseEnter={() => setIndex(i)}
             />
           ))}
-        </div>
+        </div> */}
       </div>
 
       <div className="product-detail-desc">
